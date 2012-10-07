e07826ca554efc6e90340f4268e02eb2287b4122
Travis Roberts <traviskroberts@gmail.com>
2012-10-06 22:25:19 -0500

    new server

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#DO NOT MODIFY THIS FILE
-#ANY MODIFICATIONS WILL BREAK YOUR SERVER
-let "RAM=$1*50"
-DIRS=`dirs -l`
-/bin/kill -9 `/usr/sbin/lsof "$DIRS" | grep java | sed -e 's,[ \t][ \t]*, ,g' | cut -f2 -d' '` > /dev/null 2>&1
-/usr/bin/java -server -Xmx"$RAM"M -Xms"$RAM"M -jar minecraft_server.jar nogui
